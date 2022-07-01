6eb1ceb37832d86ce2db7fe395a284f91d781efc
Gabriel Lumertz <glumertz10@gmail.com>
2022-06-30 22:20:43 -0300

    adicionando api express

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express'
+
+const api = express()
+
+api.get('/test', (req: Request, res: Response) => {
+    res.json({ success: true })
+})
+
+api.listen(3000, () => console.log('Express listening'))
